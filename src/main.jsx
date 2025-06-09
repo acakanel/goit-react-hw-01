3ae04cd9c7b39b5e8263919eb629efe95836daab
ahmet çakanel <acakanel@gmail.com>
2025-06-09 13:24:08 +0300

    commit3

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "modern-normalize";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
